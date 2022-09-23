b9fd71af48f768517e370170d511db184d1bf55b
Luanderson Alves <90487249+luandersonalvesdev@users.noreply.github.com>
2022-09-22 21:10:54 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+># Meu portfólio
+
+## ⚠️ Projeto ainda em desenvolvimento
+
+![preview](./assets/github/preview.png)
+
+## Sobre o projeto
+        Criei o portfólio no propósito de ter uma página específica para compartilhar os meus projetos ao longo do tempo.
+        Irei sempre estar atualizando de adicionando novas tecnologias, paginas e afins.
+
+># 🔗 [Acesse clicando aqui!](https://luandersonalvesdev.github.io/portfolio)
+
+## 👨‍💻 APRENDIZADO
+- Adição de classes em tags pós execução.
+- Sistema de dark mode.
+- Alteração de imagens ".svg".
+- Responsividade em 100% dos aparelhos.
+- Nav-bar para mobile.
+
+## 🛠️ TECNOLOGIAS USADAS 
+- HTML
+- CSS
+- JavaScript
+- GIT e Github
+
+## CONTATO
+
+https://linkedin.com/in/luandersonalvesdev
---
+++ assets/js/script.js
@@ -0,0 +1,40 @@
+const html = document.querySelector('html')
+const lightModeButton = document.querySelector('.checkbox-light-mode')
+const navBarListMenu = document.querySelector('.nav-bar-list-menu');
+
+/* LOGIC TO BUTTON COLOR CHANGE */
+function checkColorAndIconMenu(){
+    if(html.classList.contains('light-mode') && navBarListMenu.classList.contains('open')){
+        document.querySelector('.icon').src = "./assets/img/x-icon-black.svg";
+    }else if(html.classList.contains('light-mode') == false && navBarListMenu.classList.contains('open') == true){
+        document.querySelector('.icon').src = "./assets/img/x-icon-white.svg";
+    }else if(html.classList.contains('light-mode') == true && navBarListMenu.classList.contains('open') == false){
+        document.querySelector('.icon').src = "./assets/img/lines-icon-black.svg";
+    }else if(html.classList.contains('light-mode') == false && navBarListMenu.classList.contains('open') == false){
+        document.querySelector('.icon').src = "./assets/img/lines-icon-white.svg";
+    }
+}
+
+/* CHANGE THEME TO LIGHT MODE */
+lightModeButton.addEventListener('change', function(){
+    html.classList.toggle('light-mode')
+    checkColorAndIconMenu();
+})
+
+/* SCROLL DOWN EVENT, LITTLE NAV BAR */
+window.addEventListener('scroll', function(){
+    var header = this.document.querySelector('header')
+    header.classList.toggle('page-down', window.scrollY > 0);
+})
+
+/* SHOW MENU MOBILE */
+function menuShow(){
+    if(navBarListMenu.classList.contains('open') ){
+        navBarListMenu.classList.remove('open');
+        checkColorAndIconMenu();
+    }else{
+        navBarListMenu.classList.add('open')
+        checkColorAndIconMenu();
+    }
+}
+
