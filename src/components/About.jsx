6c6a4b25372a30e6351cfde465e1b2abf22da677
Luanderson Alves <luaoderson@gmail.com>
2024-02-17 17:46:03 -0300

    feat: about section

---
+++ src/components/About.jsx
@@ -0,0 +1,10 @@
+export default function About() {
+  return (
+    <section className="pt-32" id="about">
+      <h2 className="font-semibold dark:text-main-white-dark text-2xl">Sobre</h2>
+      <p className="dark:text-second-text-dark mt-2 text-base">
+        Desde a infância, desenvolvi uma paixão por tecnologia. Apesar de crescer em uma área rural, descobri em 2019 a acessibilidade ao estudo de programação por meio de um curso de informática na minha cidade. Este contato inicial foi se suma importância na minha vida. Em 2023, concentrando-me plenamente no meu desenvolvimento entrei e me formei na <a className="text-green-100 hover:underline" href="https://www.betrybe.com" target="_blank">Trybe</a>, desde então, busco especialização constante, dedicando-me diariamente ao aprimoramento das minhas habilidades.
+      </p>
+    </section>
+  )
+}
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,3 +1,4 @@
+import About from "../../components/About";
 import Header from "../../components/Header";
 import Profile from "../../components/Profile";
 
@@ -15,6 +16,7 @@ export default function Home() {
     >
       <Header />
       <Profile />
+      <About />
     </main>
   );
 }
