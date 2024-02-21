1cebe1f2e3b266f18ce8e26f1444e62b7cb0adfe
Luanderson Alves <luaoderson@gmail.com>
2024-02-21 18:58:59 -0300

    refactor: update about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,7 @@ export default function About() {
     <section className="md:pt-24 pt-16" id="about">
       <h2 className="font-semibold dark:text-main-white-dark md:text-xl text-lg">Sobre</h2>
       <p className="dark:text-second-text-dark mt-2 md:text-base text-sm">
-        Desde a infância, desenvolvi uma paixão por tecnologia. Apesar de crescer em uma área rural, descobri em 2019 a acessibilidade ao estudo de programação por meio de um curso de informática na minha cidade. Este contato inicial foi se suma importância na minha vida. Em 2023, concentrando-me plenamente no meu desenvolvimento entrei e me formei na <a className="text-green-100 hover:underline" href="https://www.betrybe.com" target="_blank">Trybe</a>, desde então, busco especialização constante, dedicando-me diariamente ao aprimoramento das minhas habilidades.
+        Desde a infância, minha maior paixão foi o futebol, quase sendo jogador profissional. Descobri o mundo digital entre 10-12 anos. Aos 15, trabalhei por 3 meses para comprar meu primeiro computador. De garçom a operador lotérico, sempre tive afinidade com a tecnologia. Em 2020, um curso de programação revelou minha verdadeira paixão. A pandemia me levou a trabalhos diversos, mas questionei meu caminho. Investi em aprendizado autodidata e, em 2024, me formei na <a className="text-green-100 hover:underline" href="https://www.betrybe.com" target="_blank">Trybe</a>. Desde então, venho aprimorando minhas habilidades e traçando meu caminho na programação.
       </p>
     </section>
   )
