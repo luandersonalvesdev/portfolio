698c7ca6e20c2ef51a0c973721c566765c01a8a8
Luanderson Alves <luaoderson@gmail.com>
2024-03-09 15:59:02 -0300

    style: css: find-me refactor

--- src/components/FindMe.jsx
+++ src/components/FindMe.jsx
@@ -3,11 +3,11 @@ import FindMeChess from "./FindMeChess";
 export default function FindMe() {
   return (
     <section className="md:pt-24 pt-16" id="find-me">
-      <h2 className="font-semibold dark:text-main-white-dark md:text-xl text-lg">Encontre-me</h2>
-      <p className="dark:text-second-text-dark mt-1 md:text-base text-sm">
-        Algumas outras redes que você também pode me encontrar.
-      </p>
-      <ul className="mt-5 dark:text-main-white-dark text-xl">
+      <div className="inline-block">
+        <h2 className="font-semibold dark:text-main-white-dark md:text-xl text-base">Encontre-me</h2>
+        <hr class="h-[3px] md:mt-[1px] border-0 rounded-full dark:bg-gradient-to-r from-lime-400 via-stone-400 to-fuchsia-400"></hr>
+      </div>
+      <ul className="mt-3 md:mt-5 dark:text-main-white-dark text-xl">
         <FindMeChess />
       </ul>
     </section>
--- src/svgs/BlitzChessSvg.jsx
+++ src/svgs/BlitzChessSvg.jsx
@@ -1,6 +1,6 @@
 export default function BlitzChessSvg() {
   return (
-    <svg class="h-5 w-5 md:h-6 md:w-6" viewBox="0 0 530 530" stroke="#edd012" fill="#edd012" >
+    <svg class="h-3 w-3 md:h-4 md:w-4" viewBox="0 0 510 510" stroke="#edd012" fill="#edd012" >
       <g id="SVGRepo_bgCarrier"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <g> <polygon points="392.326,200.43 325.083,200.43 423.691,0 280.813,0 133.933,298.54 210.513,298.54 115.151,538.842 "></polygon> </g> </g> </g>
     </svg>
   )
