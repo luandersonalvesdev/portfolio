2bb0ddbc340b28176f79f413c7f8f83bfb6323e8
Luanderson Alves <luaoderson@gmail.com>
2024-03-09 14:07:46 -0300

    style: css: about refactor

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,15 @@
 export default function About() {
   return (
-    <section className="md:pt-24 pt-16" id="about">
-      <h2 className="font-semibold dark:text-main-white-dark md:text-xl text-lg">Sobre</h2>
-      <p className="dark:text-second-text-dark mt-2 md:text-base text-sm">
-        Desde a infância, minha maior paixão foi o futebol, quase sendo jogador profissional. Descobri o mundo digital entre 10-12 anos. Aos 15, trabalhei por 3 meses para comprar meu primeiro computador. De garçom a operador lotérico, sempre tive afinidade com a tecnologia. Em 2020, um curso de programação revelou minha verdadeira paixão. A pandemia me levou a trabalhos diversos, mas questionei meu caminho. Investi em aprendizado autodidata e, em 2024, me formei na <a className="text-green-100 hover:underline" href="https://www.betrybe.com" target="_blank">Trybe</a>. Desde então, venho aprimorando minhas habilidades e traçando meu caminho na programação.
+    <section className="mt-14 md:mt-20" id="about">
+      <div className="inline-block">
+        <h2 className="font-semibold dark:text-main-white-dark md:text-xl text-base">Sobre</h2>
+        <hr class="h-[3px] md:mt-[1px] border-0 rounded-full dark:bg-gradient-to-r from-orange-500 via-neutral-500 to-blue-500"></hr>
+      </div>
+      <p className="dark:text-second-text-dark mt-1 md:mt-3 md:text-base text-sm text-justify">
+        Morador de Paulo Afonso, cidade do interior da Bahia, descobri a programação em 2022. Em 2023, ingressei na <a className="dark:text-green-100 hover:underline" href="https://www.betrybe.com" target="_blank">Trybe</a> e, após um ano, me formei, sendo condecorado como <a className="dark:text-green-100 hover:underline" href="https://www.credential.net/a3bb1111-88a3-47a0-88ab-50b4ebcdceb2" target="_blank">aluno destaque</a> da turma.
+      </p>
+      <p className="dark:text-second-text-dark mt-1 md:mt-2 md:text-base text-sm text-justify">
+      Com uma abordagem criativa, tenho um apreço especial por desenvolver soluções práticas e inovadoras. Busco constantemente desafios que me permitam contribuir para facilitar a vida de alguém.
       </p>
     </section>
   )
